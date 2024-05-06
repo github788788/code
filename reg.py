bcf094700ed8c5f6a512877bda56c4798b4812fc
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:59:47 -0400

    Update file via PyGithub

---
+++ reg.py
@@ -0,0 +1,10 @@
+exec(open('util.py').read())
+def reg():
+	inp = []
+	inp.append(["cop","violin78@protonmail.com"])
+	inp.append([but3,[["tab"],0,0]])
+	inp.append(["cop","Viovio1!"])
+	inp.append([but3,[["enter"],0,0]])
+	fof(inp)
+inp = []
+reg(inp)
