eaf2b2b5508619692a64c6e6513ed7d1639dd652
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:55:42 -0400

    Update file via PyGithub

---
+++ log-old.py
@@ -0,0 +1,12 @@
+exec(open('util.py').read())
+def log(inp):
+	
+	alt(1,1)
+	wri3(["violin78@protonmail.com",1])
+	key([["tab",1,0,1]])
+	wri3(["Viovio1!",1])
+	key([["enter",1,0,0]])
+		
+
+inp = []
+log(inp)
