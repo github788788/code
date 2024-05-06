427261ddd521b3669618c4af8b612de9ea9190f1
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:53:07 -0400

    Update file via PyGithub

---
+++ gom.py
@@ -0,0 +1,22 @@
+exec(open('util.py').read())
+def gom(ray):
+		#ray = ray
+
+	loc = fol(["where"])
+	whe = loc[0]
+
+	url = "https://www.google.com/maps/place/"+whe
+	url= [url]
+	"""
+	if type(url)==str:
+		url = [url]
+		"""
+	#u
+	#url = []
+	#url.append("https://www.google.com/maps/place/"+whe)
+	nu(url,1)
+	
+
+
+inp = []
+gom(inp)
