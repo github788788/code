f79244f8c10be6e8239251121477e2549f26888e
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:03:07 -0400

    Update file via PyGithub

---
+++ swe3.py
@@ -0,0 +1,14 @@
+exec(open('util.py').read())
+def swe3():
+	inp = []
+	#inp.append(act)
+	inp.append([neu3,["https://online.swedbank.se/app/ib/?ns=1&bankId=08999",1,5]])
+	#inp.append([init,["fof.txt"]])
+	inp.append(["9007305411"])
+	inp.append([but3,[["tab","tab","tab","tab","tab","tab","enter"],0,0]])
+	#inp.append([pas])
+	#inp.append([but3,[["enter"],0,0]])
+	fof3(inp)
+
+inp = []
+swe3(inp)
