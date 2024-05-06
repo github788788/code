bfd611685241aef2421120ab98591893e7c402a3
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:18:26 -0400

    Delete file

--- ban-old.py
+++
@@ -1,9 +0,0 @@
-exec(open('util.py').read())
-def ban(ray):
-	ray = ray
-	bro = "chr"
-	new([["chr","r"],ray])
-	wri("https://thenationalpulse.com/breaking/bannons-war-room-1065/",2)
-	but3([["enter"],0,0])
-inp = []
-ban(inp)
