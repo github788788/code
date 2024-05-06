35a1d69025a6b0f16a00ec73d2f7dbce51b37a65
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:03:08 -0400

    Update file via PyGithub

---
+++ swe4.py
@@ -0,0 +1,22 @@
+exec(open('util.py').read())
+def swe4():
+
+	usn = "199007305411"
+	pas = "Celcel1!"
+	inp = []
+	inp.append([neu3,["https://online.swedbank.se/app/ib/logga-in",2,5]])
+	#inp.append([init,["fof.txt"]])
+	inp.append([usn])
+	inp.append([cf2,["logga in",1]])
+	inp.append([but3,[["enter"],0,0]])
+	inp.append([cf_este3,["logga in",1,1]])
+	"""
+	inp.append([pas])
+	inp.append([but3,[["enter"],0,0]])
+	"""
+	fof3(inp)
+
+
+
+inp = []
+swe4(inp)
