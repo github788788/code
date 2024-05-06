1836bddfdce770c22a97bb1e78ff7b75072f6c7b
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:02:12 -0400

    Update file via PyGithub

---
+++ sky.py
@@ -0,0 +1,10 @@
+exec(open('util.py').read())
+def sky():
+
+	loc = "A:\Program Files (x86)\Microsoft\Skype for Desktop\Skype.exe"
+	sw(loc,0)
+	#sw("C:\\Program Files\\WindowsApps\\Microsoft.SkypeApp_15.68.96.0_x86__kzf8qxf38zg5c\\Skype\\Skype.exe",0)
+	
+	""
+inp = []
+sky(inp)
