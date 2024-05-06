bcefc7bc2343db59811474e732cbde280b2a906f
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:45:45 -0400

    Update file via PyGithub

---
+++ aum - old.py
@@ -0,0 +1,321 @@
+exec(open('util.py').read())
+def aut(ray):
+	inp = "naw"
+	num3(inp)
+
+	ray = aul()
+	one = sin()
+
+	for a,val in enumerate(one):
+		if ".com" not in val[1]:
+			if ".org" not in val[1]:
+				if ".se" not in val[1]:
+					if ".html" not in val[1]:
+						val[1] =  val[1]+".com"
+		ray.append(val)
+
+	app = []
+	for a, val in enumerate(ray):
+		tem = val[0:2]
+		tem[0] = tem[0]+"o"
+		app.append(tem)
+		print (a,tem,"yee")
+
+	ray = ray+app
+
+	#sys.exit()
+	"""
+		new = []
+		new.append(val[0]+"o")
+		for b,val2 in enumerate(val):
+			if b>0:
+				new.append(val2)
+		ray.append(new)
+		"""
+	new = []
+	for a,val in enumerate(ray):
+		tem = []
+		res = val[2:len(val)]
+		tem.append(val[0])
+		tem.append(val[1])
+		tem.append(res)
+		new.append(tem)
+
+
+
+	for a,val in enumerate(new):
+		print (a,val,"yee")
+	ray = new
+	inp = whi("to run? = ")
+
+	rep2 = []
+	#rep2.append(["own",[ur1+owb]])
+	
+	owb = ["owb"]
+	owla = []
+	#owla.append("net")
+	#owla.append("nld")
+	#owla.append("ner")
+	#owla.append("nel")
+	#owla.append("nlu")
+	owla.append("nld")	
+	owla.append("go2")
+	owla.append("pro")
+	owla.append("fac")
+	owla.append("ins")
+	owla.append("twi")
+	owla.append("cof")
+	owla.append("gco")
+	owla.append("gdo")
+	owla.append("quo")
+	owla.append("rel")
+	owb.append(owla)
+	
+	ur1 = ["ur1"]
+	ur2 = []
+	ur2.append("nru")
+	ur2.append("rum")
+	ur2.append("get")
+	ur2.append("exy")
+	ur2.append("afy")
+	ur2.append("youb")
+	ur1.append(ur2)
+
+
+	rep2.append(ur1)
+	rep2.append(owb)
+	rep2.append(["wea",["wea1","wea2"]])
+	#rep2.append(["tra",["tra1","tra2"]])
+	#las = ["las",["nlu","sto","nrd"]]
+	#rep2.append(["las",["nlu","sto","lun","ctl","nrd"]])
+	rep2.append(["las",["nlu","sto","lun","nrd"]])
+	rep2.append(["atl",["atl1","atl2"]])
+	
+	rep2.append(["nld","go2","pro","fac","ins","twi","cof","gco","gdo","quo","rel",["nld","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+	rep2.append(["ma2",["go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+	
+	rep2.append(["ma2",["go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+	
+	rep2.append(["res",["nrd","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo","nru","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+
+
+	rep2.append(["aru",["nru","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+	rep2.append(["ard",["nrd","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+	rep2.append(["alu",["nlu","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+	rep2.append(["ald",["nld","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+
+	sta1 = ["che","nld","go2","cof","pro","fac","ins","twi","gco","gdo","quo","rel","sky2"]
+	sta2 = ["nrd"]
+	ski = ["che","nld"]
+	for a,val in enumerate(sta1):
+		if val in ski:
+			continue
+		sta2.append(val+"o")
+
+	rep2.append(["sta1",sta1])
+	rep2.append(["sta2",sta2])
+	#rep2.append(["sta3",sta1,sta2])
+
+
+	#rep2.append(["sta2",["nrd","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo","sky2o"]])
+	
+
+	rep2.append(["sta",["che","nld","go2","cof","pro","fac","ins","twi","gco","gdo","quo","rel","sky2","nrd","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo","sky2o"]])	
+	rep2.append(["st3",["nld","go2o","proo","faco","inso","twio","cofo","gcoo","gdoo","quoo","relo"]])
+	
+	ope = []
+	#ope is urls, ope2 adds opening firefox,
+	#ope is just urls
+
+	"""
+	ope.append("ins")
+	ope.append("sky2")
+	ope.append("go2")
+	ope.append("caf")
+	ope.append("kee")
+	ope.append("fac")
+	ope.append("twi")
+	ope.append("pro")
+	ope.append("rel")
+
+	#tech223@proton.me
+	usn = "Emergency_Zebra_5972"
+	pas = "Zebra_59"
+
+
+	
+	"""
+
+	ope.append("pro")
+	ope.append("ins")
+	ope.append("go2")
+	ope.append("caf")
+	ope.append("kee")
+	ope.append("gma")
+	ope.append("twi")
+	ope.append("fac")
+	ope.append("rel")	
+	
+	
+	ope2 = []
+	ope2.append("che")
+	ope2.append("nld")
+	for a, val in enumerate(ope):
+		ope2.append(val)
+	ope2.append("nrd")
+	ope3 = []
+	ope3.append("nrd")
+	for a, val in enumerate(ope):
+		app = val+"o"
+		ope2.append(app)
+		ope3.append(app)
+	dis(ope2)
+	rep2.append(["ope",ope2])
+	rep2.append(["ope2",ope3])
+
+	#sye()
+
+
+	rep2.append(["st4",["che","nld","go2","cof","pro","fac","rel","sky2","nrd","go2o","proo","faco","cofo","relo","sky2o"]])	
+	
+
+
+	run = []
+
+
+	for a,val in enumerate(inp):
+		if val=="own":
+			run.append("ur1")
+			run.append("owb")
+
+		if val=="ow2":
+			run.append("owb")
+			run.append("owb")
+			run.append("mle")
+			#run.append("las")
+		else:
+			run.append(val)
+
+	print (run)
+	
+	new = []
+	for a,val in enumerate(run):
+		hit = 0
+		for b,val2 in enumerate(rep2):
+			if val==val2[0]:
+				for c,val3 in enumerate(val2[1]):
+					new.append(val3)
+				hit = 1
+		if hit==0:
+			new.append(val)
+
+	print (new)
+
+	def di1(inp):
+		for a,val in enumerate(inp):
+			print (a,val,"yee")
+	di1(new)
+	#urn = []
+	sen = []
+	sea = []
+	urr = []
+	def pop(inp):
+		bac = []
+		for a,val in enumerate(inp):
+			bac.append(val)
+		return bac
+	nex = ["net","ner","nel","nru","nlu","nrd","nld"]
+	alr = 0
+	print ("new",new)
+	hit =0
+	pgc = 0
+	for a,val in enumerate(new):
+		if val not in nex:
+			pgc = pgc+1
+
+	print (new,pgc)
+	var = 0
+	for a,val in enumerate(new):
+		for b,val2 in enumerate(ray):
+			sec = val2[0]
+			if val==sec:
+				url = val2[1]
+				if a==len(new)-1 and hit==0:
+					#sen.append([aln,[1,1]])
+					sen.append([aln,[1,1]])	
+				if val not in nex:
+					if len(url)>0:
+						urr.append(url)
+					for c,val3 in enumerate(val2[2]):
+						print (val3)
+						sea.append(val3)
+					if var<pgc-1:
+					#if var<pgc-2:
+						sea.append([hod3,["ctrl","pagedown",1,0]])
+						var = var+1
+				print ("sea",sea)
+				if val in nex:
+					hit=hit+1
+				if val in nex or a==len(new)-1:
+					if len(urr)>0:
+						print ("urr",urr)
+						if len(urr)==1:
+							sen.append([cot,[urr,0,1]])
+						if len(urr)>1:
+							#if "sta3" in inp:
+								#sen.append([opa,[urr,0]])
+							sen.append([opt2,[urr,0]])
+					for c,val3 in enumerate(sea):
+						sen.append(val3)
+					if val in nex:
+						hit = hit+1
+						for c,val3 in enumerate(val2[2]):
+							sen.append(val3)
+					urr = []
+					sea = [] 
+					
+	for a,val in enumerate(sen):
+	#if val[0]=="net":
+		print (a,val,"yee")
+	
+	def run4(inp):
+		fir = 0
+		tex =[]
+		beg = 0
+		for a,val in enumerate(inp):
+			print ("val",val)
+			if type(val) is list and len(val) is 2:
+				val[0](val[1])
+			print (a,val)
+			#if type(val) is list and len(val) is 2:
+			#	val[0](val[1])
+			if type(val) is str:
+
+				if beg==0:
+					for b,val2 in enumerate(inp):
+						if type(val2) is str:
+							tex.append(val2)
+					out = ""
+					for b,val2 in enumerate(tex):
+						out = out+val2+"\n"
+					fil = "tex.txt"
+					f= open(fil,"w")
+					f.write(out)
+					f.close()
+					"""
+					if len(tex)>0:
+						sw(fil,1)
+						"""
+					beg=1 
+				cop([tex,fir])
+				fir = 1
+		print (tex)
+	
+	
+
+	run4(sen)
+inp = []
+
+exec(open('aul.py').read())
+exec(open('sin.py').read())
+aut(inp)
