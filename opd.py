98a7f6f6b126589d5e27914c0f4d12e00fd708af
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:58:10 -0400

    Update file via PyGithub

---
+++ opd.py
@@ -0,0 +1,13 @@
+exec(open('util.py').read())
+def opd(inp):
+
+	fil =str(input("file = "))
+	loc = "C:\\Users\\-\\Documents\\"
+	loc = loc+fil+".txt"
+
+	#sw("C:\\Users\\-\\"#0c",1)
+	sw(loc,2)
+
+	#res7("rd")
+inp = []
+opd(inp)
