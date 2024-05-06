1a85d0c451c2ee93cbd8f5a94096cd8926498d20
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:58:41 -0400

    Update file via PyGithub

---
+++ pg2.py
@@ -0,0 +1,15 @@
+exec(open('util.py').read())
+def pg2(li,te):
+	pu(len(li)-1)
+	le = 0
+	for a in range(0,len(li)):
+		 if "google.com/flights" in li[a]:
+		 	le = le+1
+	for a in range(0,le):
+		#cf_etste(te)
+		cf_ee("price graph")	
+		if a<le-1:
+			hod3(["ctrl","pagedown",1,0])
+
+inp = []
+pg2(inp)
