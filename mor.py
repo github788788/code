1b95fc4c879c5e8ce5da2f36cff2927324d26a67
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:56:44 -0400

    Update file via PyGithub

---
+++ mor.py
@@ -0,0 +1,25 @@
+exec(open('util.py').read())
+def mor(inp):
+	add = inp[0]
+	#fil = inp[0]
+	con = rt(fil)
+	ide = ""
+	if con!=ide:
+		new = open("mor.txt","w")
+		new.write(ide)
+		new.close()
+		#time.sleep(1) 
+	if con==ide:
+		add = inp
+
+		new = open("mor.txt","w")
+		new.write(ide)
+		new.close()
+		
+
+
+		out =ide
+
+
+		"""inp = []
+mor(inp)
