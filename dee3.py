7417e31e380a22fd6c2bfeeadc6afc1cbf8110e6
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:48:16 -0400

    Update file via PyGithub

---
+++ dee3.py
@@ -0,0 +1,14 @@
+exec(open('util.py').read())
+def dee3():
+	#tex = ray[0]
+	tex = input("search term = ")
+	at(1,1)
+	cf_e("upgrade")
+	hod3(["shift","tab",3,0])
+	wri(tex,1)
+	but(["enter"],0,2)
+	cf_e("folders")
+	but2([[4,"tab"],[1,"space"]])
+	#but(["enter"],0,2)
+inp = []
+dee3(inp)
