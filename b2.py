a6790716ddf01c61263b1e11ce748db9ae1c91bd
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:18:20 -0400

    Delete file

--- b2.py
+++
@@ -1,9 +0,0 @@
-exec(open('util.py').read())
-def b2(ray,between_wait,end_wait):
-	for a in range(0,len(ray)):
-		pyautogui.press(ray[a])
-		time.sleep(between_wait)
-	time.sleep(end_wait)
-
-inp = []
-b2(inp)
