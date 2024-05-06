ad97cd41da34d98eaa7e7a7a9c973a565e56916b
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:53:34 -0400

    Update file via PyGithub

---
+++ gto.py
@@ -0,0 +1,11 @@
+exec(open('util.py').read())
+def gto():
+	qui = 0
+	tow = []
+	while(qui<1):
+		nto = str(input("0 to end, town to add = "))
+		if nto=="0":
+			break
+		tow.append(nto) 
+	return towinp = []
+gto(inp)
