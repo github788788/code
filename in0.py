dac5ef432c2d6c2dd5979a0a8381fae00d33f657
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:54:34 -0400

    Update file via PyGithub

---
+++ in0.py
@@ -0,0 +1,49 @@
+exec(open('util.py').read())
+def in0(ray):
+
+	alt(1,1)
+	"""
+	#hod3(["ctrl","r",1,5])
+	cf3(["messages",1])
+	but3([["enter","enter","esc","tab"],0,0])
+	hod3(["shift","tab",1,0])
+	wri3(["0c",1])
+	but3([["enter"],0,0])
+	"""
+
+	cf("search",1)
+	key([["enter",2,0,0]])
+	cf_etst(["search"])
+	wri("0c",1)
+	key([["enter",1,0,3]])
+	
+
+	cf("4.0.",1)
+	"""
+	com =[]
+	com.append(["esc",1,0,0])
+	com.append(["tab",2,0,0])
+	com.append(["space",2,0,0])
+	key(com)
+	"""
+	
+	key([["esc",1,0,0],["tab",2,0,0],["space",2,0,0]])
+	cf("newest",1)
+	key([["esc",1,0,0],["tab",1,0,0],["space",1,0,0]])
+	for a in range(0,4):
+		key([["tab",3,0,0],["space",1,0,0]])
+	sys.exit()
+		
+
+	def fri():
+		cf_ee33(["inbox",2])
+		cf("newest",1)
+		but3([["esc"],0,0])
+		hod3(["shift","tab",2,0])
+		but3([["enter"],0,1])
+		but3([["up","up","enter"],0,1])
+		hod3(["shift","tab",2,0])
+		but3([["space","t"],1,0])
+		#cf_ee33(["trash",2newest])
+inp = []
+in0(inp)
