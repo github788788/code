67596f426857cbf558787ab79b04eaa035c2bf87
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:46:41 -0400

    Update file via PyGithub

---
+++ chb.py
@@ -0,0 +1,14 @@
+exec(open('util.py').read())
+def chb(con):
+
+	ray = []
+	ray.append(["fir","fir"])
+	ray.append(["chr","chr"])
+	con = rt(con)
+	for a,val in enumerate(ray):
+		if val[0] in con:
+			bro = val[1]
+	return bro
+
+inp = []
+chb(inp)
