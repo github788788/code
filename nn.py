99fe58a9603171c71fe89129a791647c43b2667d
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:57:43 -0400

    Update file via PyGithub

---
+++ nn.py
@@ -0,0 +1,17 @@
+exec(open('util.py').read())
+def nn(be,af):
+		#ga = int(((af/be)-1)*100000)
+		#ga = int(((af/be)-1)*100000)
+		ga = float(af)/float(be)
+		ga = ga-1
+		ga = ga*10000
+		ga = int(ga)
+		ga = float(ga)
+		ga = ga/100
+		#ga = float(ga/100)
+
+
+		#ga = float(ga)
+		#ga = ga/1000
+		return gainp = []
+nn(inp)
