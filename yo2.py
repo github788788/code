5e825dd798beafa8a7ce14f2da5cc08e7a7015b1
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:06:03 -0400

    Update file via PyGithub

---
+++ yo2.py
@@ -0,0 +1,111 @@
+exec(open('util.py').read())
+def yo2(ray):
+	new = []
+	nam = "yos"
+	sea = ["youtube search"]
+	sea = fo2(sea)
+	sea = sea[0][1]
+	print (sea)
+	url = ["https://www.youtube.com/results?search_query="+sea]
+	return url
+	#bac.append([])
+
+	def app(inp):
+		ray = inp[0]
+		sea = ["youtube search"]
+		sea = fo2(sea)
+		sea = sea[0][1]
+		url = ["https://www.youtube.com/results?search_query="+sea]
+		mor = ["url",url]
+		ray.append(mor)
+		return ray
+
+	for a,val in enumerate(ray):
+		fir = val[0]
+		#sec = val[1]
+		if fir=="url":
+			url.append(val[1])
+		
+
+
+	num4([url,1])
+	sys.exit()
+
+
+	new = []
+	nam = "yos"
+	new.append(nam)
+	new.append([yos])
+	new.append(nam)
+	new.append(nam)
+
+	ray = []
+	new = []
+	nam = "yos"
+	fur = []
+	fur.appe+nd(yos)
+	fur.append([fol2,["youtube search"]])
+	fin = []
+	fin.append([yos])
+
+
+	new.append(nam)
+	new.append(fur)
+	new.append(fin)
+
+	for a,val in enumerate(fur):
+		val()
+	for a,val in enumerate(fin):
+		if a==0:
+			bac = fur[len(fur)-1]
+			sea = bac[0]
+
+		seq2.append(val)
+
+
+
+	def rur(ray):
+		for a,val in enumerate(ray):
+			if a==0:
+				url = []
+				for b,val2, in enumerate(ray):
+
+					req = "req"
+			if len(val)==1:
+				val = val[0]
+				if type(val)==function:
+					val()
+				if type(val)==str:
+					if "www" in val:
+						uro.append(val)
+					if "www" not in val:
+						tex = tex+val+"\n"
+
+			if len(val)==2:
+				fir = val[0]
+				if type(fir)==str:
+					continue
+				val[0](val[1])
+
+
+	new.append(nam)
+
+
+
+	sea = ["youtube search"]
+	sea = fo2(sea)
+	sea = sea[0][1]
+	print (sea)
+	url = ["https://www.youtube.com/results?search_query="+sea]
+	num4([url,1])
+	sys.exit()
+
+
+
+	usn = "9007305411"
+	new = [nam,url]
+	new.append(usn)
+	new.append([but3,[["enter"],0,0]])
+	#ray.append(new)
+	inp = []
+yo2(inp)
