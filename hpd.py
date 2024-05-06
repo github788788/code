3c50bfac2b0958586e4cd508ebe0cfe91eca4544
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:54:25 -0400

    Update file via PyGithub

---
+++ hpd.py
@@ -0,0 +1,15 @@
+exec(open('util.py').read())
+def hpd(inp):
+
+	alt(1,1)
+	cf(["what os"])	
+	key([["esc",1,0,0],["tab",1,0,0],["enter",1,0,1]])
+	key([["tab",3,0,1],["enter",1,0,1]])
+	key([["tab",2,0,1]])
+	key([["enter",1,0,1]])
+	key([["tab",2,0,1],["enter",1,0,1]])
+	key([["tab",1,0,1],["enter",1,0,1]])
+
+	
+inp = []
+hpd(inp)
