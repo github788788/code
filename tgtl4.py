b14b15c5512d2e6223abe68f2cafa8a3ebf183a0
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:04:23 -0400

    Update file via PyGithub

---
+++ tgtl4.py
@@ -0,0 +1,12 @@
+exec(open('util.py').read())
+def tgtl4():
+	at(1,1)
+	com = []
+	com.append([init,["fof.txt"]])
+	com.append([cf_et5,["email",1,1]])
+	com.append(["violin78@protonmail.com"])
+	com.append([but3,[["tab"],0,0]])
+	com.append(["Viovio1!"])
+	com.append([but3,[["enter"],0,0]])
+	fof3(com)inp = []
+tgtl4(inp)
