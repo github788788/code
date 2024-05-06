e57e88bd0c0a7909ccdaaebc12ef7fdb2b38180a
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:04:11 -0400

    Update file via PyGithub

---
+++ tex2.py
@@ -0,0 +1,742 @@
+exec(open('util.py').read())
+def tex2():
+	ray = []
+	
+	new = []
+	nam = "pat"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	new.append([net,[0,["chr","r"]]])
+	ray.append(new)
+
+	
+
+	new = []
+	nam = "meb"
+	new = [nam,url]
+	ray.append(new)
+
+	new = []
+	nam = "tb"
+	url = "https://docs.google.com/spreadsheets/d/1E9umtnbKku8wBp-7sB3btIkVcvsE6x4QOjHTl2SgrWk/edit?usp=drive_web&ouid=104465905925623639959"
+	new = [nam,url]
+	ray.append(new)
+
+
+
+	new = []
+	nam = "net"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	new.append([net,[0,["chr","r"]]])
+	ray.append(new)
+
+	new = []
+	nam = "ner"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	new.append([net,[0,["chr","r"]]])
+	ray.append(new)
+
+	new = []
+	nam = "nel"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	new.append([net,[0,["chr","s"]]])
+	ray.append(new)
+
+
+	new = []
+	nam = "nru"
+	new = [nam,url]
+	new.append([ne72,["ru"]])
+	ray.append(new)
+
+	new = []
+	nam = "ctl"
+	new = [nam,url]
+	new.append([hod3,["ctrl","9",1,0]])
+	ray.append(new)
+
+
+	new = []
+	nam = "nlu"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	#new.append([ne72,[0,["chr","lu"]]])
+	new.append([ne72,["lu"]])
+	ray.append(new)
+
+	new = []
+	nam = "nrd"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	#new.append([ne72,[0,["chr","rd"]]])
+	new.append([ne72,["rd"]])
+	
+	ray.append(new)
+
+	new = []
+	nam = "nld"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	#new.append([ne72,[0,["chr","ld"]]])
+	new.append([ne72,["ld"]])	
+	ray.append(new)
+
+
+	#alr(["fir"])
+
+	new = []
+	nam = "che"
+	new = [nam,url]
+	#new.append(spe)
+	#new.append([but3,[["enter"],0,0]])
+	#new.append([ne72,[0,["chr","ld"]]])
+	new.append([alr,["fir"]])	
+	ray.append(new)
+
+
+	new = []
+	nam = "mlu"
+	new = [nam,url]
+	new.append([mot,["lu"]])
+	ray.append(new)
+
+	new = []
+	nam = "mld"
+	new = [nam,url]
+	new.append([mot,["ld"]])	
+	ray.append(new)
+
+	new = []
+	nam = "mru"
+	new = [nam,url]
+	new.append([mot,["ru"]])	
+	ray.append(new)
+
+	new = []
+	nam = "mrd"
+	new = [nam,url]
+	new.append([mot,["rd"]])	
+	ray.append(new)
+
+	new = []
+	nam = "mri"
+	new = [nam,url]
+	new.append([mot,["r"]])	
+	ray.append(new)
+
+	new = []
+	nam = "mle"
+	new = [nam,url]
+	new.append([mot,["l"]])	
+	ray.append(new)
+
+	new = []
+	nam = "csn"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "med"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	#https://www.tinkercad.com/login
+	new = []
+	nam = "tin"
+	new = [nam,url]
+	new.append([wai,[]])
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append([cf_est,["password",1,1]])	
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "tra"
+	new = [nam,url]
+	new.append([wai,[]])
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append([cf_est,["password",1,1]])	
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	new = []
+	nam = "wix"
+	new = [nam,url]
+	new.append([wai,[]])
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append([cf_est,["password",1,1]])	
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+
+	new = []
+	nam = "wor"
+	new = [nam,url]
+	new.append([wai,[]])
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append([cf_est,["password",1,1]])	
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+
+
+	new = []
+	nam = "rel"
+	
+	#tech223@proton.me
+	
+	
+
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([cf_etst,["username"]])
+	new.append("usn")
+	new.append([cf_etst,["password"]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "spo"
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([hod3,["ctrl","a",1,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	new.append([sle,[3]])
+	new.append([cf_ee5,["web player",0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "fsk"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "lab"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "sk2"
+	pas ="vioviovio1"
+	new = [nam,url]
+	new.append([sta,["skype.lnk"]])
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "gom"
+	new = [nam,url]
+	new.append([sle,[1]])
+	new.append([gom,[]])
+	ray.append(new)
+
+	new = []
+	nam = "swe"
+	new = [nam,url]
+	new.append([sle,[1]])
+	new.append("9007305411")
+	new.append([cf3,["logga in",1]])
+	new.append([but3,[["enter","esc"],1,0]])
+	new.append([hod3,["shift","tab",1,0]])
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+	
+	new = []
+	nam = "unh"
+	amo = "1"
+	ema = "violin78@protonmail.com"
+	tel = "0731431617"
+	num = "199007305411"
+	car = "5168 1531 3323 9073"
+	dat = "04-24"
+	cod = "818"
+
+	new = [nam,url]
+	#new.append([sle,[3]])
+	new.append([sle,[6]])
+	new.append([cf_est3,["valfritt",1,3]])
+	new.append([but3,[["down","down","tab"],0,1]])
+	new.append(amo)
+	new.append([but3,[["tab"],0,0]])
+	new.append(ema)
+	new.append([but3,[["tab"],0,0]])
+	new.append(tel)
+	new.append([but3,[["tab"],0,0]])
+	new.append(num)
+	new.append([but3,[["tab","tab","enter"],0,2]])
+	new.append([cf_est3,["kortbe",1,1]])
+	new.append([but3,[["down","tab","tab","enter"],0,10]])
+	new.append([but3,[["tab"],0,0]])
+	new.append(car)
+	new.append([but3,[["tab"],0,0]])
+	new.append(dat)
+	new.append([but3,[["tab"],0,0]])
+	new.append(cod)
+	ray.append(new)
+	
+	new = []
+	nam = "unh2"
+	amo = "1"
+	ema = "violin78@protonmail.com"
+	tel = "0731431617"
+	num = "199007305411"
+	car = "5168 1577 6580 9269"
+	dat = "11-26"
+	cod = "124"
+
+	new = [nam,url]
+	#new.append([sle,[3]])
+	new.append([sle,[6]])
+	new.append([cf_est3,["valfritt",1,3]])
+	new.append([but3,[["down","down","tab"],0,1]])
+	new.append(amo)
+	new.append([but3,[["tab"],0,0]])
+	new.append(ema)
+	new.append([but3,[["tab"],0,0]])
+	new.append(tel)
+	new.append([but3,[["tab"],0,0]])
+	new.append(num)
+	new.append([but3,[["tab","tab","enter"],0,2]])
+	new.append([cf_est3,["kortbe",1,1]])
+	new.append([but3,[["down","tab","tab","enter"],0,10]])
+	new.append([but3,[["tab"],0,0]])
+	new.append(car)
+	new.append([but3,[["tab"],0,0]])
+	new.append(dat)
+	new.append([but3,[["tab"],0,0]])
+	new.append(cod)
+	ray.append(new)
+
+	nam = "afe"
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([cf_etm3,["route",1]])
+	new.append([sle,[2]])
+	new.append(["Rostock - Trelleborg"])
+	new.append([sle,[2]])
+	new.append([but3,[["down","enter","enter"],1,0]])
+	ray.append(new)
+	
+	nam = "cra"
+	new = [nam,url]
+	new.append([sle,[2]])
+	#new.append([cfb,["email",[["esc",1,0,1],["tab",1,0,1]]]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+	
+	new = []
+	nam = "mwh"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([sle,[1]])
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+	
+	new = []
+	nam = "atl1"
+	new = [nam,url]
+	new.append([cf_ee5,["login",0,2]])
+	new.append([but3,[["tab","tab"],0,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	def atl3(inp):
+		hod3(["ctrl","pageup",1,1])
+		sle([5])
+		cf_este3(["email me",1,2])
+		hod3(["ctrl","pagedown",1,1])
+
+
+	nam = "atl2"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	#new.append([but3,[["enter"],0,0]])
+	#new.append([atl3,[]])
+	new.append([hod3,["ctrl","pageup",1,1]])
+	new.append([sle,[5]])
+	new.append([cf_este3,["email me",1,2]])
+	new.append([hod3,["ctrl","pagedown",1,1]])
+
+	ray.append(new)	
+	
+	new = []
+	nam = "ant"
+	#url = "https://member.anthem.com/secure/claims"
+	new = [nam,url]
+	
+	new.append([sle,[4]])
+	new.append([cf_et3,["usern",1,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,1
+		]])
+	#new.append([sle,[1]])
+	new.append("pas")
+	#new.append([sle,[1]])
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "sky"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "sky2"
+	#url = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1651348909&rver=7.1.6819.0&wp=MBI_SSL&wreply=https%3A%2F%2Flw.skype.com%2Flogin%2Foauth%2Fproxy%3Fclient_id%3D572381%26redirect_uri%3Dhttps%253A%252F%252Fweb.skype.com%252FAuth%252FPostHandler%26state%3Dbd4d509f-29bd-4470-8942-0f633699bce0&lc=1033&id=293290&mkt=en-US&psi=skype&lw=1&cobrandid=2befc4b5-19e3-46e8-8347-77317a16a5a5&client_flight=ReservedFlight33%2CReservedFlight67"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+
+	nam = "go1"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "go2"
+	url = "https://accounts.google.com/signin/v2/identifier?service=wise&passive=true&continue=http%3A%2F%2Fdrive.google.com%2F%3Futm_source%3Den&utm_medium=button&utm_campaign=web&utm_content=gotodrive&usp=gtd&ltmpl=drive&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+	#usn = "media788788"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "pro"
+	new = []
+	new = [nam,url]
+	new.append([sle,[2]])
+	new.append([cf_et3,["email",1,0]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "low"
+	new = []
+	new = [nam,url]
+	new.append([sle,[2]])
+	new.append([cf_et3,["low",1,0]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "prn"
+	new = []
+	new = [nam,url]
+	new.append([cf_et3,["email",1,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "prv"
+	new = []
+	new = [nam,url]
+	new.append([cf_et3,["email",1,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "prc"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "prc2"
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+
+	nam = "fac"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "iex"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	new.append([key,[["tab",2,0,0]]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+
+	nam = "tar"
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([but3,[["tab","tab","tab","tab"],0,0]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+
+	nam = "quo"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	#new.append([cf_et5,[usn,2,2]])
+	#new.append([cf_et5,[usn,1,0]])
+	new.append([cf_et5,[usn,1,1]])	
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "twi"
+	new = []
+	new = [nam,url]
+	#new.append([cf_et5,["apple",1,1]])
+	#cf2(["apple",1])
+	new.append([cf2,["sign up",1]])
+	new.append([key,[["esc",1,0,1]]])
+	new.append([hod3,["shift","tab",4,1]])
+	new.append("usn")
+	new.append([but3,[["enter"],0,2]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+	
+
+	nam = "ins"
+	new = []
+	new = [nam,url]
+	new.append([cf_et5,["phone",1,0]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "inc"
+	new = []
+	new = [nam,url]
+	#new.append([cf_ee5,["log in",1,1]])
+	#cf_etste3(["log in",0,2])
+	new.append([sle,[3]])
+	new.append([cf_etste3,["log in"]])
+	new.append([sle,[2]])
+	new.append([key,[["tab",2,0,1]]])
+	
+	#key([["esc",1,0,0],["tab",2,0,0],["space",2,0,0]])
+	
+	#cf_etste3(["log in"])
+	
+	#new.append([but3,[["tab","tab"],0,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,0]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)	
+
+	nam = "eba"
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([cf_et5,["account",1,1]])
+	new.append("usn")
+	new.append([but3,[["enter"],0,2]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,2]])
+	ray.append(new)
+
+	nam = "pla"
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([key,[["tab",3,0,1]]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,1]])
+	ray.append(new)
+
+
+	nam = "har"
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([cf_et5,["email",1,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,2]])
+	new.append("pas")
+	new.append([key,[["tab",2,0,1],["space",1,0,3]]])
+	new.append([key,[["tab",4,0,1],["enter",1,0,0]]])
+	ray.append(new)
+
+
+	nam = "git"
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	#new.append([cf_et5,["account",1,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,2]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	nam = "aws"
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	#new.append([cf_et5,["account",1,1]])
+	new.append("usn")
+	new.append([but3,[["tab"],0,2]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+
+
+	nam = "cvs"
+	
+	new = []
+	new = [nam,url]
+	new.append([sle,[3]])
+	new.append([cf_et5,["address",1,1]])
+	new.append("book56@protonmail.com")
+	new.append([but3,[["enter"],0,2]])
+	new.append([but3,[["tab"],0,1]])
+	new.append("Booboo1!")
+	new.append([but3,[["enter"],0,2]])
+	ray.append(new)
+
+	nam = "nas"
+	new = [nam,url]
+	new.append([cf_et5,["email",1,1]])
+	new.append("usn")
+	new.append([key,[["tab",1,0,1],["enter",1,0,1]]])
+	new.append("pas")
+	new.append([key,[["tab",1,0,1],["enter",1,0,1]]])
+	ray.append(new)
+
+	nam = "ama"
+	new = []
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["enter"],0,3]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+	
+	nam = "ed1"
+	#url = "file:///A:/Users/-/0c/webf.html"
+	new = []
+	new = [nam,url]
+	new.append([cf_ee5,["login",0,2]])
+	ray.append(new)
+
+	nam = "ed2"
+	#new = []
+	new = [nam,url]
+	new.append([hod3,["ctrl","r",1,2]])
+	ray.append(new)
+
+	new = []
+	nam = "lin"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+
+	new = []
+	nam = "wal"
+	new = [nam,url]
+	new.append("usn")
+	new.append([but3,[["tab"],0,1]])
+	new.append("pas")
+	new.append([but3,[["enter"],0,0]])
+	ray.append(new)
+	
+	#"https://gettr.com/"
+	return ray
+inp = []
+#tex2()"
