f6c8a5c6c5b6a1f31d20507ba327d7db1e87446f
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:05:35 -0400

    Update file via PyGithub

---
+++ vir.py
@@ -0,0 +1,21 @@
+exec(open('util.py').read())
+def vir():
+	url = []
+	url.append("https://www.google.com/maps/place/haynesville, va")
+	url.append("https://www.google.com/maps/place/fredericksburg, va")
+	url.append("https://www.google.com/maps/place/northern virginia, va")
+	url.append("https://www.google.com/maps/place/washington dc, va")
+	nu(url,1)
+	pgu(len(url),0)
+	time.sleep(6)
+	for a in range(0,len(url)):
+		if a>0:
+			time.sleep(3)
+		cf_ete2("satell"
+
+			)
+		if a<len(url)-1:
+			hod3(["ctrl","pagedown",1,0])
+	hod3(["ctrl","1",1,0])
+inp = []
+vir(inp)
