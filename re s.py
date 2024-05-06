ce5e873dd4448475579171620b98d66f9d610255
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:59:30 -0400

    Update file via PyGithub

---
+++ re s.py
@@ -0,0 +1,11 @@
+exec(open('util.py').read())
+def re s(ra):
+	#time.sleep(wa)
+	pyautogui.keyDown("win")
+	for a in range(0,len(ra)):
+		but([ra[a]],0,0)
+	pyautogui.keyUp("win")
+	but(["esc","end"],0,0)
+	"""
+inp = []
+re s(inp)
