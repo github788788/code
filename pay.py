52746b578d47b3d661ef527eff7b41f1475ccb66
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:58:32 -0400

    Update file via PyGithub

---
+++ pay.py
@@ -0,0 +1,21 @@
+exec(open('util.py').read())
+def pay():
+
+	url = "https://www.paypal.com/us/signin"
+	usn = "gocubs7@protonmail.com"
+	pas = "Vvbd63Vvbd63"
+	#inp.append([cf_et4(text)])	
+	inp = []
+	inp.append([neu3,[url,2,5]])
+	inp.append([but3,[["tab"],0,1]])
+	#inp.append([cf_et5,["account",1,1]])
+	inp.append([usn])
+	inp.append([but3,[["enter"],0,4]])
+	inp.append([pas])
+	inp.append([but3,[["enter"],0,0]])
+	fof3(inp)
+
+
+
+	inp = []
+pay(inp)
