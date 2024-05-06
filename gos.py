20b476f00d0bfcfb4808325bb846c5f4f1b818b1
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:53:20 -0400

    Update file via PyGithub

---
+++ gos.py
@@ -0,0 +1,29 @@
+exec(open('util.py').read())
+def gos(ray):
+	ray = ray
+	app = ges(rt("gos2"),"\n")
+
+	print (app)
+
+	urr = []
+	for a,val in enumerate(app):
+		"""
+		if len(val)==0:
+			continue
+		"""
+		if a<1:
+			continue
+		sea = app[0]+" "+val
+		url = "https://www.google.com/search?q="+sea
+		urr.append(url)
+		""
+	print (urr)
+	#sys.exit()
+
+
+	#num4([urr,1])
+	opt2([urr,1])
+
+	print (app)
+inp = []
+gos(inp)
