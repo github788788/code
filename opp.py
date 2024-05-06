bfdd37a4cb6b850647dd5ea7d7bbc5767e63d507
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 16:58:19 -0400

    Update file via PyGithub

---
+++ opp.py
@@ -0,0 +1,12 @@
+exec(open('util.py').read())
+def opp():
+	spe =str(input("spe = "))
+	loc =str(input("loc = "))
+	neu4("file:///C:/Users/-/Documents/Lightshot/"+spe+".jpg",1,0)
+	#inp = []
+	#inp.append("location")
+	#inp = ask(inp)
+	#loc = inp[0][1]
+	res8(loc)
+#opp()inp = []
+opp(inp)
