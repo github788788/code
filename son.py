51b8d2de2a4d3cc799fb91c381cadbc233296a55
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-06 17:02:21 -0400

    Update file via PyGithub

---
+++ son.py
@@ -0,0 +1,17 @@
+exec(open('util.py').read())
+def son(ray):
+
+	app = ges(rt("son2"),"\n")
+	ray = []
+	for a,val in enumerate(app):
+		url = "https://www.songkick.com/search?utf8=%E2%9C%93&type=initial&query="
+		url = url+val
+		url = url+",va"
+		ray.append(url)
+
+	#fredericksburg#%2C+va&commit=	
+
+	num4([ray,1])
+	
+inp = []
+son(inp)
