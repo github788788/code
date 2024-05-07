d6541ba126eb3b75ff24a0d67e1bed154f346870
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 16:18:44 -0400

    file updated! yo!

--- upload.py
+++ upload.py
@@ -1,14 +1,18 @@
 exec(open('util.py').read())
 def upload(inp):
 
+	mp4_file_name = "fulton_300k_votes"
+	description = "Over 300,000 ballot images lost Georgia 2020 recount (Fulton county)"
+
+	media_file = mp4_file_name
+	if ".mp4" not in mp4_file_name:
+		media_file = mp4_file_name+".mp4"
 	files = []
-	files.append(["election.mp4","Election Fraud!"])
-	
+	files.append([media_file,description])	
 	cwd = os.getcwd()
 	downloads_directory = cwd.replace("code","Downloads")	
 	rumble_url = "https://rumble.com/upload.php"
 	twitter_url = "https://twitter.com/compose/post"
-
 	#alt_win81(["Chrome"]):
 	alt_win81(["Chrome",1])
 	for a,val in enumerate(files):
