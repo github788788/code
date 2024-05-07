0e9727d4e1f0b9b05cdec5dc65293da5f438ac54
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:38:13 -0400

    file updated! yo!

--- nav.py
+++ nav.py
@@ -368,6 +368,22 @@ def nav(inp):
 	new.append([gom,[]])
 	lib.append(new)
 
+	"""
+	usn = "199007305411"
+	pas = "Celcel1!"
+	inp = []
+	inp.append([neu3,["https://online.swedbank.se/app/ib/logga-in",2,5]])
+	#inp.append([init,["fof.txt"]])
+	inp.append([usn])
+	inp.append([cf2,["logga in",1]])
+	inp.append([but3,[["enter"],0,0]])
+	inp.append([cf_este3,["logga in",1,1]])
+
+
+	fof3(inp)
+"""
+
+
 	new = []
 	nam = "swe"
 	new = []
