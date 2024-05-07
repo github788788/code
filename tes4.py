d2572a390e1c2898e065c0600bf2ff529408a39f
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:32:44 -0400

    file updated! yo!

--- tes4.py
+++ tes4.py
@@ -1,4 +1,7 @@
 exec(open('util.py').read())
 
-token = load_data(["github_token.txt"])
-print(token)
+alt_win81(["Sublime",2])
+
+for a in range(0,50):
+	hold_button(["ctrl","f4",1,0])
+	key([["n",1,0,0]])
