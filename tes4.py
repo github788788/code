e65efed66496566180bdb8aa738111addfbafd02
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:47:30 -0400

    file updated! yo!

--- tes4.py
+++ tes4.py
@@ -2,6 +2,8 @@ exec(open('util.py').read())
 
 alt_win81(["Sublime",2])
 
-for a in range(0,50):
+number_of_clicks = 20
+
+for a in range(0,number_of_clicks):
 	hold_button(["ctrl","f4",1,0])
 	key([["n",1,0,0]])
