88ce598047a61e7c85dad44abe667aac94783fb0
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:32:27 -0400

    Delete file

--- szt.py
+++
@@ -1,9 +0,0 @@
-exec(open('util.py').read())
-def szt(inp):
-	cwd = os.getcwd()
-	out = cwd+".7z"
-	print (cwd,out)
-	with py7zr.SevenZipFile(out, 'w') as archive:
-		    archive.writeall(cwd)
-inp = []
-szt(inp)
