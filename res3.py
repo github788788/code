c9c18dd061dc9635d462107e86cb2c8bb1e38738
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:46:39 -0400

    Delete file

--- res3.py
+++
@@ -1,9 +0,0 @@
-exec(open('util.py').read())
-def res3(ra):
-	#time.sleep(wa)
-	pyautogui.keyDown("win")
-	for a in range(0,len(ra)):
-		but([ra[a]],0,0)
-	pyautogui.keyUp("win")
-	inp = []
-res3(inp)
