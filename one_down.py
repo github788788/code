af64ebf30cd7ae8bf688da6378f61574370f2cc4
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:58:54 -0400

    Delete file

--- one_down.py
+++
@@ -1,7 +0,0 @@
-exec(open('util.py').read())
-def one_down(first,second,wait):
-	pyautogui.keyDown(first)
-	pyautogui.press(second)
-	pyautogui.keyUp(first)
-	time.sleep(wait) inp = []
-one_down(inp)
