342e31c43b9aef8758830eb1113cf1299ffc0a7e
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:31:05 -0400

    Delete file

--- two_down.py
+++
@@ -1,8 +0,0 @@
-exec(open('util.py').read())
-def two_down(first,second,wait):
-	pyautogui.keyDown(first)
-	pyautogui.press(second)
-	pyautogui.press(second)
-	pyautogui.keyUp(first)
-	time.sleep(wait) inp = []
-two_down(inp)
