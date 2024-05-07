e5db07b1f06c4f83a68e412577e3470295e5b217
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 18:29:02 -0400

    Delete file

--- but.py
+++
@@ -1,9 +0,0 @@
-exec(open('util.py').read())
-def but(ray,between_wait,end_wait):
-	for a in range(0,len(ray)):
-		pyautogui.press(ray[a])
-		time.sleep(between_wait)
-	time.sleep(end_wait)
-
-inp = []
-but(inp)
