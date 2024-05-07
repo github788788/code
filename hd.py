754606bd4259ead52de87547bb4ed2f61d43d29b
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 18:21:39 -0400

    Delete file

--- hd.py
+++
@@ -1,9 +0,0 @@
-exec(open('util.py').read())
-def hd(first,second,clicks,wait):
-	pyautogui.keyDown(first)
-	for a in range(0,clicks):
-		pyautogui.press(second)
-	pyautogui.keyUp(first)
-	time.sleep(wait) 
-inp = []
-hd(inp)
