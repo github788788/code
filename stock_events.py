9bdcc86ca21ba36dd6a0c26b3f7f6d9ce19ea7c9
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:38:10 -0400

    file created! yo!

---
+++ stock_events.py
@@ -0,0 +1,15 @@
+exec(open('util.py').read())
+def sto(inp):
+	"""
+	weekly-unemployment, jobless claims
+	monthly-gdp,fed,cpi
+
+	fed - gdp,announce,
+	bls = cpi,unemployment
+	dol = jobless claims
+
+	"""
+
+
+inp = []
+sto(inp)
