8dc4ad4e72ab88e04fb0f88d7e3a851be367062d
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-07 17:32:43 -0400

    file updated! yo!

--- nav.py
+++ nav.py
@@ -226,6 +226,8 @@ def nav(inp):
 	new.append(run)
 	lib.append(new)	
 
+
+
 	new = []
 	nam = "wix"
 	new = []
@@ -468,6 +470,48 @@ def nav(inp):
 	new.append([but3,[["enter"],0,0]])
 	lib.append(new)	
 	
+	"""
+	url = "https://mychart.mwhc.com/mychart/Billing/Details?ID=WP-24BZ0G8iQup-2B0YpR6dV6umeQ-3D-3D-24HyCPRBVi9Tsd0DWVi57-2Bv0ZSpX9HMaYHmhQB7Yh07WI-3D&Context=WP-24kaa7uki37RKmJYXx9C3DdQ-3D-3D-24w-2B8Fh5dBnlLOcSy9SzR8Eu1gObDNVjvEi30vpI14S7s-3D"	
+	usn = "mwhos"
+	pas = "mwhos123"
+
+
+
+	usn = "cello34"
+	pas = "Celcel1!"
+	inp = []
+	inp.append([neu3,["https://myvhc.virginiahospitalcenter.com/MyChartPRD/Authentication/Login?",2,5]])
+
+
+exec(open('util.py').read())
+def tapl():
+	url = "https://www.flytap.com/en-us/login"
+	usn = "violin78@protonmail.com"
+	pas = "Viovio1!"
+	#inp.append([cf_et4(text)])	
+	inp = []
+	inp.append([neu3,[url,2,5]])
+	inp.append([cf_et5,["account",1,1]])
+	inp.append([usn])
+	inp.append([but3,[["tab"],0,0]])
+	inp.append([pas])
+	inp.append([but3,[["enter"],0,0]])
+	fof3(inp)
+
+
+
+
+
+inp = []
+tapl(inp)
+
+
+	"""
+
+
+
+
+
 	new = []
 	nam = "mwh"
 	new = []
