6eea0301c12df5ee6ace63b73cb715c41da77e4c
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-21 11:45:34 -0400

    file created! yo!

---
+++ open_operations.py
@@ -0,0 +1,143 @@
+exec(open('util.py').read())
+
+storage = []
+
+new = []
+new.append("drive")
+new.append("https://keep.google.com/")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([copy_paste2,["media788788",1]])
+chrome_operations.append([key,[["enter",1,0,5]]])
+chrome_operations.append([key,[["esc",1,0,1]]])
+chrome_operations.append([copy_paste2,["Medmed1!",1]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+firefox_operations.append([])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("logins")
+new.append("https://docs.google.com/spreadsheets/d/1pveuTlsMCB5vKhEhwBA28IidJPkOokVk/edit#gid=1983355307")
+chrome_operations = []
+firefox_operations = []
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("reddit_questions")
+new.append("https://docs.google.com/spreadsheets/d/1qrySuja-rx_LoHcXIoLAidd1C92hqyeouNv2O7iWnDs/edit#gid=0")
+chrome_operations = []
+firefox_operations = []
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("keep")
+new.append("https://keep.google.com/")
+chrome_operations = []
+firefox_operations = []
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("gmail")
+new.append("https://mail.google.com/")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([hold_button,["alt","d",1,1]])
+chrome_operations.append([copy_paste2,["gmail.com",1]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("proton.me")
+new.append("https://account.proton.me/login")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([copy_paste2,["violin78",1]])
+chrome_operations.append([key,[["tab",1,0,0]]])
+chrome_operations.append([copy_paste2,["viovio",0]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+firefox_operations.append([])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("reddit_login")
+new.append("https://www.reddit.com/login/")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([copy_paste2,["media788788",1]])
+chrome_operations.append([key,[["enter",1,0,5]]])
+chrome_operations.append([copy_paste2,["Medmed1!",1]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+firefox_operations.append([])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("reddit_notifications")
+new.append("https://www.reddit.com/notifications")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([copy_paste2,["media788788",1]])
+chrome_operations.append([key,[["enter",1,0,5]]])
+chrome_operations.append([copy_paste2,["Medmed1!",1]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+firefox_operations.append([])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("twitter_login")
+new.append("https://twitter.com/i/flow/login")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([copy_paste2,["media788788",1]])
+chrome_operations.append([key,[["enter",1,0,5]]])
+chrome_operations.append([copy_paste2,["Medmed1!",1]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+firefox_operations.append([])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("messenger")
+new.append("https://www.messenger.com")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([copy_paste2,["media788788",1]])
+chrome_operations.append([key,[["enter",1,0,5]]])
+chrome_operations.append([copy_paste2,["Medmed1!",1]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+firefox_operations.append([])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+new = []
+new.append("chatgpt")
+new.append("https://auth.openai.com/authorize?client_id=TdJIcbe16WoTHtN95nyywh5E4yOo6ItG&scope=openid+email+profile+offline_access+model.request+model.read+organization.read+organization.write&response_type=code&redirect_uri=https%3A%2F%2Fchatgpt.com%2Fapi%2Fauth%2Fcallback%2Flogin-web&audience=https%3A%2F%2Fapi.openai.com%2Fv1&device_id=aaa1f3ab-045e-4b14-869d-1b7da8b2f8ac&prompt=login&state=sAYCYO4I26abKym8hhHX3fTHfjmYa-CrMlm-w7kTJKw&code_challenge=ul1ssINYqpK70A9EDPA-HbbXAmsvRgZzN6I6V0SGKFg&code_challenge_method=S256")
+chrome_operations = []
+firefox_operations = []
+chrome_operations.append([copy_paste2,["media788788",1]])
+chrome_operations.append([key,[["enter",1,0,5]]])
+chrome_operations.append([copy_paste2,["Medmed1!",1]])
+chrome_operations.append([key,[["enter",1,0,0]]])
+firefox_operations.append([])
+new.append(chrome_operations)
+new.append(firefox_operations)
+storage.append(new)
+
+
