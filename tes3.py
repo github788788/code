70b920cf722f6ae0af889df91f4e19ae7e9d8811
github788788 <166743743+github788788@users.noreply.github.com>
2024-05-21 06:09:57 -0400

    file updated! yo!

--- tes3.py
+++ tes3.py
@@ -1,26 +1,7 @@
 exec(open('util.py').read())
 
-import pygetwindow as gw
-import pyautogui
+array = [1,6,3,8,4]
 
-def get_window_names():
-    return [window.title for window in gw.getAllWindows()]
+print(array.index(8))
 
-def activate_window(window_title):
-    try:
-        window = gw.getWindowsWithTitle(window_title)
-        if window:
-            window[0].activate()
-        else:
-            print(f"Window '{window_title}' not found.")
-    except Exception as e:
-        print(f"Error: {e}")
-
-# Example usage:
-window_names = get_window_names()
-pri(window_names)
 end()
-print("Available Windows:", window_names)
-
-# Activate a specific window by its title
-activate_window("Calculator")
