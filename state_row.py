62b83622a12223d63d83fb50aeade5adc7d76d5f
github788788 <166743743+github788788@users.noreply.github.com>
2024-06-10 17:41:01 -0400

    file updated! yo!

--- state_row.py
+++ state_row.py
@@ -1,12 +1,12 @@
 
 row_code = """
-        <div id="if3ql" class="gjs-grid-row">
-                <div id="i2j44" class="gjs-grid-column">
-                        <div id="isldp">__state__</div>
+        <div id="__state_row__" class="gjs-grid-row">
+                <div id="__state_col__" class="gjs-grid-column">
+                        <div id="__state_name__">__state__</div>
                 </div>
-                <div id="i8fr2" class="gjs-grid-column">
-                        <div id="i8g3g" class="gjs-grid-row">
-                                <div id="iwndj" class="gjs-grid-column">
+                <div id="__vid_col__" class="gjs-grid-column">
+                        <div id="__vid_row__" class="gjs-grid-row">
+                                <div id="__vid1_col__" class="gjs-grid-column">
                                         <div id="ir1a7"><iframe frameborder="0"
                                                         src="https://rumble.com/embed/__vid1_code__/?pub=3i4h9q"
                                                         id="__vid1__"></iframe></div>
