3a67aaccce1b203f4364834a5b56752fabb012ef
github788788 <166743743+github788788@users.noreply.github.com>
2024-06-10 15:57:50 -0400

    file created! yo!

---
+++ rumble_gen.py
@@ -0,0 +1,35 @@
+exec(open('util.py').read())
+def rumble_gen(inp):
+	rumble_urls = []
+	new = []
+	new.append("arizona")
+	new.append("lankford")
+	new.append("130 thousand illegal votes were cast in Nevada")
+	new.append("https://rumble.com/embed/v4spnbx/?pub=3i4h9q")
+	rumble_urls.append(new)
+
+	new = []
+	new.append("georgia")
+	new.append("Joseph Rossi")
+	new.append("Georgia Recount Violated Georgia Election Law")
+	new.append("https://rumble.com/embed/v4r928i/?pub=3i4h9q")
+	rumble_urls.append(new)
+
+	new = []
+	new.append("__state__")
+	new.append("__person__")
+	new.append("__description__")
+	new.append("__rumble_embed_url__")
+	rumble_urls.append(new)
+
+	new = []
+	new.append("__state__")
+	new.append("__person__")
+	new.append("__description__")
+	new.append("__rumble_embed_url__")
+	rumble_urls.append(new)
+
+
+	
+inp = []
+rumble_gen(inp)
