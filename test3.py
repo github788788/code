e64860ce441536afac220f1e72f3cc126e20ea74
github788788 <166743743+github788788@users.noreply.github.com>
2024-06-10 18:32:37 -0400

    file updated! yo!

--- test3.py
+++ test3.py
@@ -1,39 +1,8 @@
 exec(open('util.py').read())
 def test3(inp):
 	
-
-	base_html = """
-	
-        <div id="__new_row__" class="gjs-grid-row">
-                <div id="__new_col__" class="gjs-grid-column">
-                        <div id="__new_row__" class="gjs-grid-row">
-                                <div id="__new_col__" class="gjs-grid-column">
-                                        <div id="__state__">__which_state__</div>
-                                </div>
-                        </div>
-                </div>
-                <div id="__new_col__" class="gjs-grid-column">
-                        <div id="__new_row__" class="gjs-grid-row">
-                                <div class="gjs-grid-column">
-                                        <div id="iocj3"><iframe frameborder="0"
-                                                        src="https://rumble.com/embed/v4qvth5/?pub=3i4h9q"
-                                                        id="arizona1-2"></iframe></div>
-                                </div>
-                                <div id="i2owo" class="gjs-grid-column">
-                                        <div id="i6lauy"><iframe frameborder="0"
-                                                        src="https://rumble.com/embed/v4qvth5/?pub=3i4h9q"
-                                                        id="arizona2"></iframe></div>
-                                </div>
-                                <div class="gjs-grid-column">
-                                        <div id="imt6xh"><iframe frameborder="0"
-                                                        src="https://rumble.com/embed/v4qvth5/?pub=3i4h9q"
-                                                        id="arizona3"></iframe></div>
-                                </div>
-                        </div>
-                </div>
-        </div>
-        """
-
+	exec(open('state_row.py').read())
+	print(row_code)
 
 
 inp = []
