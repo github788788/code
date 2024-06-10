5c6eb083dbb182b8b3f78923feaa051cc7fca187
github788788 <166743743+github788788@users.noreply.github.com>
2024-06-10 17:30:24 -0400

    file updated! yo!

--- test3.py
+++ test3.py
@@ -1,9 +1,40 @@
 exec(open('util.py').read())
 def test3(inp):
 	
-	inputs = whi("to open? = ")
-	print(inputs)
-	print(len(inputs))
+
+	base_html = """
 	
+        <div id="__new_row__" class="gjs-grid-row">
+                <div id="__new_col__" class="gjs-grid-column">
+                        <div id="__new_row__" class="gjs-grid-row">
+                                <div id="__new_col__" class="gjs-grid-column">
+                                        <div id="__state__">__which_state__</div>
+                                </div>
+                        </div>
+                </div>
+                <div id="__new_col__" class="gjs-grid-column">
+                        <div id="__new_row__" class="gjs-grid-row">
+                                <div class="gjs-grid-column">
+                                        <div id="iocj3"><iframe frameborder="0"
+                                                        src="https://rumble.com/embed/v4qvth5/?pub=3i4h9q"
+                                                        id="arizona1-2"></iframe></div>
+                                </div>
+                                <div id="i2owo" class="gjs-grid-column">
+                                        <div id="i6lauy"><iframe frameborder="0"
+                                                        src="https://rumble.com/embed/v4qvth5/?pub=3i4h9q"
+                                                        id="arizona2"></iframe></div>
+                                </div>
+                                <div class="gjs-grid-column">
+                                        <div id="imt6xh"><iframe frameborder="0"
+                                                        src="https://rumble.com/embed/v4qvth5/?pub=3i4h9q"
+                                                        id="arizona3"></iframe></div>
+                                </div>
+                        </div>
+                </div>
+        </div>
+        """
+
+
+
 inp = []
 test3(inp)
