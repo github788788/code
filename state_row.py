619adae3667d279b30a75a22151940391eb42d19
github788788 <166743743+github788788@users.noreply.github.com>
2024-06-10 18:32:36 -0400

    file updated! yo!

--- state_row.py
+++ state_row.py
@@ -7,17 +7,17 @@ row_code = """
                 <div id="__vid_col__" class="gjs-grid-column">
                         <div id="__vid_row__" class="gjs-grid-row">
                                 <div id="__vid1_col__" class="gjs-grid-column">
-                                        <div id="ir1a7"><iframe frameborder="0"
+                                        <div id="ir1a7"><iframe id="__vid1__"
                                                         src="https://rumble.com/embed/__vid1_code__/?pub=3i4h9q"
                                                         id="__vid1__"></iframe></div>
                                 </div>
                                 <div class="gjs-grid-column">
-                                        <div id="iauuv"><iframe frameborder="0"
+                                        <div id="iauuv"><iframe id="__vid2__"
                                                         src="https://rumble.com/embed/__vid2_code__/?pub=3i4h9q"
                                                         id="__vid2__"></iframe></div>
                                 </div>
                                 <div class="gjs-grid-column">
-                                        <div id="irpeg"><iframe frameborder="0"
+                                        <div id="irpeg"><iframe id="__vid3__"
                                                         src="https://rumble.com/embed/__vid3_code__/?pub=3i4h9q"
                                                         id="__vid3__"></iframe></div>
                                 </div>
