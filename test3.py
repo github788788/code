4b4c9b426ad7eff7d7b2f3f432bbf6e2f7a813d3
github788788 <166743743+github788788@users.noreply.github.com>
2024-06-20 19:12:01 -0400

    file updated! yo!

--- test3.py
+++ test3.py
@@ -1,9 +1,8 @@
 exec(open('util.py').read())
 def test3(inp):
 	
-	#exec(open('state_row.py').read())
-	#print(row_code)
-	subprocess_open([0,"https://rumble.com/v3by09g-warroom-live.html","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"])
+	subprocess.Popen(["C:\\Program Files\\ConEmu\\ConEmu64.exe","python open.py"])
+
 
 inp = []
 test3(inp)
