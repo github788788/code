5e84e1440c9161469b6d42fea5a5314ce8e96d43
github788788 <166743743+github788788@users.noreply.github.com>
2024-08-10 15:37:43 -0400

    file updated! yo!

--- test2.py
+++ test2.py
@@ -1,9 +1,17 @@
 exec(open('util.py').read())
 #exec(open('test.py').read())
 
-stocks = load_data(["earn_stocks.xls"])
-stocks2 = []
-for a,val in enumerate(stocks):
-	stocks2.append(val[0])
-stocks = stocks2
-pri(stocks)
+earn_list = os.listdir("earn")
+for a,val in enumerate(earn_list):
+    if "historical_prices_yahoo" in val:
+        array = load_data(["earn\\"+val])
+        array.append(array[0])
+        array = array[1:len(array)]
+        array = array[::-1]
+        #save_file = "earn\\"+val
+        save_file = val
+        write_data([save_file,array])
+        
+        #pri(array[0:20])
+
+
