1279d736e335f1e4210415b28f3f1682680b39d6
github788788 <166743743+github788788@users.noreply.github.com>
2024-08-12 13:02:07 -0400

    file updated! yo!

--- test2.py
+++ test2.py
@@ -1,23 +1,51 @@
 exec(open('util.py').read())
 #exec(open('test.py').read())
 
-file_name = "earn_aug_19" #without extension
-text = load_data([file_name+".txt"])
+import pandas as pd
 
+# Define the data
 
-text =text.replace("\n\n","\n")
-values = nex4([text,"\n","\n"])
-for a,val in enumerate(values):
-    values[a] = val.replace("\n","")
-values2 = []
-for a,val in enumerate(values):
-    if len(val)>5:
-        continue
-    if len(val)==0:
-        continue
-    
-    if [val] not in values2:
-        values2.append([val])
-values = values2
-pri(values)
-write_data([file_name+".xls",values])  
+data = load_data(["earn_500_final.xls"])
+
+"""
+data = [
+    ["Name", "Age", "City"],
+    ["Alice", 30, "New York"],
+    ["Bob", 25, "San Francisco"],
+    ["Charlie", 35, "Los Angeles"]
+]
+"""
+
+# Convert data to DataFrame
+df = pd.DataFrame(data[1:], columns=data[0])
+
+# Generate HTML table
+html_table = df.to_html(index=False, classes='sortable', border=0)
+
+# HTML template with DataTables
+html_template = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sortable Table</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {{
+            $('.sortable').DataTable();
+        }});
+    </script>
+</head>
+<body>
+    <h1>Sortable Table</h1>
+    {html_table}
+</body>
+</html>
+"""
+
+# Save to file
+with open('sortable_table.html', 'w') as file:
+    file.write(html_template)
+
+print("HTML file with sortable table created successfully.")
