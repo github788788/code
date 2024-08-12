6120d78b297fba0c1cb71c9dc17b6d3ceb6fb1ed
github788788 <166743743+github788788@users.noreply.github.com>
2024-08-12 19:08:31 -0400

    file updated! yo!

--- earn_exp2.js
+++ earn_exp2.js
@@ -7,17 +7,71 @@ const { exec } = require('child_process');
 // Define the port and create the Express app
 const app = express();
 const port = 3000; // You can change this to any available port number
-
 // Middleware to handle JSON requests
 app.use(express.json());
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Middleware to parse URL-encoded bodies (for form submissions)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Basic route
 //data = load_data("earn_500_final.xls")
 base_html = load_data("earn_html.html")
 //console.log(base_html)
+/*
 app.get('/', (req, res) => {
     res.send(base_html);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '', 'earn_html.html'));
+});
+*/
+
+app.route('/')
+    .get((req, res) => {
+        // Serve the HTML file (assuming you have index.html in the 'public' directory)
+        res.sendFile(path.join(__dirname, '','earn_html.html'));
+    })
+    .post((req, res) => {
+        const postReceived = req.body.symbol;
+        //const postAltered = postReceived.toUpperCase();
+        data = load_data("earn_500_final.xls")
+        console.log(data)
+        console.log(postReceived)
+
+        end()
+
+        for (let i = 0; i < 5; i++) {
+            console.log(i);
+        }
+
+        // Load data from the Excel file
+        //const continueReverse = loadData(path.join(__dirname, 'earn_500_final.xls'));
+        //console.log('continueReverse:', continueReverse);
+
+        const fileToLoad = path.join(__dirname, 'earn', `${postReceived}_prices_around_earnings.xls`);
+        const prices = loadData(fileToLoad);
+        // console.log('prices:', prices);
+
+        let toSendBack = [];
+        for (const val of continueReverse) {
+            const symbol = val[1];
+            if (postAltered === symbol) {
+                toSendBack.push(val);
+                break;
+            }
+        }
+        
+        console.log('toSendBack =', toSendBack);
+        toSendBack.push(prices);
+
+        // Send the data back as JSON
+        res.json(toSendBack);
+    });
+
 
 // Start the server
 app.listen(port, async () => {
