d3a54dc11d41581b1344148b054de6fcbe035525
github788788 <166743743+github788788@users.noreply.github.com>
2024-10-15 14:58:53 -0400

    file created! yo!

---
+++ test5.py
@@ -0,0 +1,7 @@
+exec(open('util.py').read())
+def test5(inp):
+	
+	subprocess_open([5,"file:///C:/Users/--/code/open2.1.html","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"])	
+	
+inp = []
+test5(inp)
