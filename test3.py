8c739227c8d3fc8e365466fa59ebcd49b52c9dbb
github788788 <166743743+github788788@users.noreply.github.com>
2024-10-15 14:59:09 -0400

    file updated! yo!

--- test3.py
+++ test3.py
@@ -1,6 +1,24 @@
 exec(open('util.py').read())
+
+import subprocess
+
+# Path to the Chrome executable
+chrome_path = r"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+
+# Open a new window
+subprocess.run([chrome_path, '--incognito', '--new-window'])
+
+
+
+
+
+
+
+
+"""
 import yfinance as yf
 stock = yf.Ticker("MSFT")
 hist = stock.history(period="5y")
 array = dataframe_to_list2([hist])
 pri(array)
+"""
